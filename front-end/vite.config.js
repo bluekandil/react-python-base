713ed04b33c647ff6136e9ca519527b5796fa960
BlueKandil <25916618+bluekandil@users.noreply.github.com>
2025-09-16 14:13:36 +1000

    Update vite.config.js

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  // Load the env file based on current mode
+  const env = loadEnv(mode, path.resolve())
+
+  return {
+    plugins: [react()],
+    define: {
+      __API_BASE_URL__: JSON.stringify(env.VITE_API_BASE_URL),
+    },
+  }
 })
