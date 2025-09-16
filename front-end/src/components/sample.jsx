cc60cb416b1a786f2202e048f221350c5190ede5
BlueKandil <25916618+bluekandil@users.noreply.github.com>
2025-09-16 14:13:50 +1000

    Create sample.jsx

---
+++ front-end/src/components/sample.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { helloApiUrl } from '../util/urls';
+
+
+function Test() {
+
+  // Function to handle button click and fetch data from the API
+  const [apiResponse, setApiResponse] = React.useState(null);
+
+  const handleClick = () => {        
+      fetch(helloApiUrl)
+      .then(response => response.json())
+      .then(data => setApiResponse(data))
+      .catch(error => console.error('Error:', error));
+  }    
+
+  return (
+    <div>      
+      {/* <p>Test Component Loaded.</p> */}
+      <button type="primary" onClick={() => handleClick() }>API TEST</button>
+      {apiResponse && (
+        <div>
+          <p><pre>{apiResponse.message}</pre></p>         
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Test;
